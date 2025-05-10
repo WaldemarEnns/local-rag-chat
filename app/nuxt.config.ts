3a699a873f65da66b407a65993c52fdea21f1aab
WaldemarEnns <ennsdesigns96@gmail.com>
2025-05-10 08:08:43 +0200

    Configures tailwindcss for the nuxt app

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
+  css: ['~/assets/css/main.css'],
   devtools: { enabled: true },
   modules: ['@nuxt/eslint', '@nuxt/test-utils'],
   vite: {
